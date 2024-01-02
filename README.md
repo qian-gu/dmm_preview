32e33945c000ba5cafdc33d4df74124301553b75
Qian Gu <imqiangu@gmail.com>
2024-01-03 02:00:42 +0800

    fix: support more serial number

--- README.md
+++ README.md
@@ -1,21 +1,31 @@
 # dmm_preview
 
-Generate DMM preview link according to serial number.
+Generate DMM preview link according to serial numbers.
 
 ## Usage
 
 ```bash
-# get preview url for a single video
+# Get preview url for a single serial number.
 ./dmm_preview.py JUFE-440
 
-# get preview url for multiple videos
-./dmm_preview.py JUFE-440 BLK-616
+# Get preview url for multiple serial number, note the serial number is case insensitive.
+./dmm_preview.py jufe-440 BLK-616
 ```
 
-## Checked List
+## Supported SN List
 
-- [x] BLK-xxx
-- [x] BBTU-xxx
-- [x] GVH-xxx
-- [x] JUFE-xxx
-- [x] MIAA-xxx
+| SN        | Example  |
+| --------- | -------- |
+| AVSA-xxx  | avsa-281 |
+| BBTU-xxx  | bbtu-053 |
+| BLK-xxx   | blk-616  |
+| FCDC-xxx  | fcdc-128 |
+| GENU-xxx  | genu-005 |
+| GVH-xxx   | gvh-537  |
+| JUFE-xxx  | jufe-440 |
+| JUQ-xxx   | juq-372  |
+| MEYD-xxx  | meyd-863 |
+| MIAA-xxx  | miaa-915 |
+| ROE-xxx   | roe-175  |
+| WAAA-xxx  | waaa-323 |
+| VEC-xxx   | vec-432  |
--- dmm_preview.py
+++ dmm_preview.py
@@ -1,17 +1,37 @@
 #!/usr/bin/env python
 import sys
 
+base_url = 'https://cc3001.dmm.co.jp/litevideo/freepv/'
+
+# serial number dictionary
+sn_dicts = {
+        'avsa' : 2,
+        'bbtu' : 0,
+        'blk'  : 2,
+        'fcdc' : 2,
+        'genu' : 0,
+        'gvh'  : 0,
+        'jufe' : 2,
+        'juq'  : 2,
+        'meyd' : 2,
+        'miaa' : 5,
+        'roe'  : 2,
+        'waaa' : 2,
+        'vec'  : 0,
+        }
+
 def parse(sn):
     prefix, number = sn.lower().split('-')
-    if prefix in ['gvh', 'bbtu']:
-        pad_len = 0
+    if prefix in sn_dicts:
+        pad_len = sn_dicts[prefix]
     else:
-        pad_len = 5 - len(number)
+        pad_len = 2
+        print("WARNING: no records for prefix", prefix, ", using default pad length, to be validated!")
     pad = pad_len * '0'
     return prefix, pad, number
 
 def get_preview_url(sn):
-    base_url = 'https://cc3001.dmm.co.jp/litevideo/freepv/'
+    # Parse to get elements to assemble the URL.
     prefix, pad, number = parse(sn)
     full_sn = prefix + pad + number
     suffix = prefix[0] + '/' + prefix[0:3] + '/' + full_sn + '/' + full_sn + '_mhb_w.mp4'
